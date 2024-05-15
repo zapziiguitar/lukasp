29aae8e43c2c34f93d0a31809f9da6162a228c84
zapzii <lukaspdks@gmail.com>
2024-05-15 15:35:25 +0300

    added grappling hook and crosshair
    will fix grappling hook bug later

---
+++ 3D Movement/Assets/RotateGun.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGun : MonoBehaviour
+{
+    public GrapplingGun grappling;
+
+    private Quaternion desiredRotation;
+    private float rotationSpeed = 5f;
+    void Update()
+    {
+        if (!grappling.IsGrappling()) 
+        {
+            desiredRotation = transform.parent.rotation;
+        }  
+        else
+        {
+            desiredRotation = Quaternion.LookRotation(grappling.GetGrapplePoint() - transform.position);
+        }
+
+        transform.rotation = Quaternion.Lerp(a: transform.rotation, b: desiredRotation, t: Time.deltaTime * rotationSpeed);
+    }
+    
+}
